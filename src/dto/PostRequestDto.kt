9a2d764e8ae10dd837a782cf09ee094fbc1baacd
vadimarsh <59313785+vadimarsh@users.noreply.github.com>
2020-08-30 21:00:28 +0800

    Add README.md

--- src/dto/PostRequestDto.kt
+++ src/dto/PostRequestDto.kt
@@ -3,7 +3,7 @@ package com.example.dto
 import com.example.model.PostTypes
 
 data class PostRequestDto(
-    val id: Long,
+    val id: Long = -1L,
     val author_id: Long,
     val content: String = "new post",
     val posttype: PostTypes = PostTypes.POSTBASIC
